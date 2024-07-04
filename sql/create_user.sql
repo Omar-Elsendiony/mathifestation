77d84e18c2cdcac871068c09646c6a5f0c898466
Omar Elsendiony <omar.sendiony@gmail.com>
2024-07-04 21:09:18 +0300

    change

--- sql/create_user.sql
+++ sql/create_user.sql
@@ -1,3 +1,3 @@
-CREATE USER 'maths_dev'@'localhost' IDENTIFIED BY '';
+CREATE USER 'maths_dev'@'localhost' IDENTIFIED BY 'maths_dev_pwd';
 GRANT ALL PRIVILEGES ON maths_dev_db.* TO 'maths_dev'@'localhost';
 FLUSH PRIVILEGES;
