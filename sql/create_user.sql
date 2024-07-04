43f7d92fc2922b75e6da3ba635da7fc47e867424
Omar Elsendiony <omar.sendiony@gmail.com>
2024-07-04 21:12:23 +0300

    change

--- sql/create_user.sql
+++ sql/create_user.sql
@@ -1,3 +1,5 @@
+DROP USER IF EXISTS 'maths_dev'@'localhost';
 CREATE USER 'maths_dev'@'localhost' IDENTIFIED BY 'maths_dev_pwd';
 GRANT ALL PRIVILEGES ON maths_dev_db.* TO 'maths_dev'@'localhost';
 FLUSH PRIVILEGES;
+
