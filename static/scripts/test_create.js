cc2d7dbfdc2a93792f026b82efa289e55c8db337
Omar Elsendiony <omar.sendiony@gmail.com>
2024-07-16 10:34:10 +0300

    afraid loss

---
+++ static/scripts/test_create.js
@@ -0,0 +1,172 @@
+
+var selectedItem = null;
+const questions = new Array();
+const questions_options = new Array();
+const correct_choice = new Array();
+
+var question_index = 0
+var questions_total = 1
+
+
+
+function optionSelected(element) {
+    if (selectedItem != null) {
+        selectedItem.style.borderColor = "aliceblue";
+        selectedItem.style.backgroundColor = "white";
+    }
+    element.style.backgroundColor = "lightgreen";
+    element.style.borderColor = "green";
+    selectedItem = element;
+}
+
+
+
+setTimeout(() => {
+
+document.getElementById('startButton').addEventListener('click',
+    function(event) {
+        document.querySelector('.rules_data').style.display = "none";
+        document.querySelector('.meta_data').style.display = "block";
+});
+
+document.getElementById('startQuestion').addEventListener('click',
+    function(event) {
+        document.querySelector('.meta_data').style.display = "none";
+        document.querySelector('.question_creation').style.display = "block";
+        // updateQuestion("add")
+        document.getElementById('numberQuestion').innerHTML = "Question " + (question_index + 1) + " of " + (questions_options.length + 1);
+
+});
+
+function updateQuestion(status) {
+    if (status == "add") {
+        question_index += 1;
+    } else {
+        question_index -= 1;
+    }
+
+    document.getElementById('numberQuestion').innerHTML = "Question " + (question_index + 1) + " of " + (questions_options.length + 1);
+}
+
+
+document.getElementById('add').addEventListener('click',
+    function(event) {
+        document.querySelector('.meta_data').style.display = "none";
+        document.querySelector('.question_creation').style.display = "block";
+
+        var question = document.querySelector('.que_text').value;
+        console.log(question)
+        if (question == ""){
+            alert("Please fill the question")
+            return;
+        }
+
+        var correct = null;
+        var options = document.querySelectorAll('.option');
+        for (let i = 0; i < options.length; i++) {
+            if (options[i].parentNode.style.backgroundColor == "lightgreen") {
+                correct = options[i].value;
+            }
+        }
+
+        inputs = document.querySelectorAll('.option input')
+
+
+        for (let i = 0; i < inputs.length; i++) {
+            console.log(inputs[i].value)
+            if (inputs[i].value == "") {
+                alert("Please fill all the options")
+                return;
+            }
+        }
+
+        let inputs_values = new Array();
+        for (let i = 0; i < inputs.length; i++) {
+            inputs_values.push(inputs[i].value)
+        }
+
+        questions.push(question)
+        questions_options.push(inputs_values)
+        correct_choice.push(correct)
+
+        console.log(questions_options)
+        console.log(questions)
+
+        updateQuestion("add")
+
+        /// reset the input fields ///
+        document.querySelector('.que_text').value = ""
+        for (let i = 0; i < inputs.length; i++) {
+            inputs[i].value = ""
+        }
+
+});
+
+}, 1000);
+
+
+
+
+
+
+/////////////////////////////////////////////////////
+//////////////// ANIMATE RUBIK //////////////////////
+////////////////////////////////////////////////////
+
+setTimeout(() => {
+    var start = true
+    var direction = "down";
+    function downToUp() {
+        $("#floatingRubik").animate({ top: '-=50%' }, 10000, function() {
+                animateElement(); 
+        })};
+
+
+    function upToDown() {
+        $("#floatingRubik").animate({ top: '+=50%' }, 10000, function() {
+                animateElement();
+        })};
+
+    function animateElement() {
+        var parentElement = document.getElementsByClassName('playing_rubik')[0];
+        visibilityParent = parentElement.checkVisibility();
+        // console.log(visibilityParent)
+        if (visibilityParent == false){
+            return
+        }
+
+        var element = document.getElementById('floatingRubik');
+
+        // console.log(screen.height)
+        
+        var position = element.getBoundingClientRect();
+        var x = position.left;
+        var y = position.top;
+        // console.log(y)
+        
+        if (start == true) {
+            start = false;
+            upToDown();
+        }
+        else{
+            // console.log("else")
+            if (y >= 0.4 * screen.height ) {
+                direction = "up";
+                // rightToLeft();
+            }
+            if (y <= 0.3 * screen.height) {
+                direction = "down"
+                // leftToRight();
+            }
+            if (direction == "down") {
+                upToDown();
+            }
+            else{
+                downToUp();
+            }
+        }
+    }
+    // Start the animation
+    animateElement();
+
+}, 50);
